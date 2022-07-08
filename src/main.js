ef3adb15a3082404d24277e63d2f8b13232ffc4c
Alex Hytha <ahytha@relaynetwork.com>
2022-07-08 13:49:35 -0400

    added healthcheck

--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
 const fastify = require('fastify')({ logger: true });
 
 fastify.get('/', async (request, reply) => {
-  console.log("server hit");
-  reply.s
-  return 'Hello World';
+  console.log("healthcheck");
+  reply.send();
 });
 
+fastify.get('/helloworld', async (request, reply) => {
+  console.log("server hit");
+  reply.send('Hello World');
+});
 
 const start = async () => {
   try {
